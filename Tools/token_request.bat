98d7ed4698a8683e9a8910f01e5eac2a8403d6e1
danndato <danieltova97@gmail.com>
2025-02-04 18:40:31 -0600

    Estableciendo produccion

--- Tools/token_request.bat
+++
@@ -1,4 +0,0 @@
-curl -X POST "https://id.twitch.tv/oauth2/token" \ -H "Content-Type: application/x-www-form-urlencoded" \ -d "client_id=9qacsqfnmlhvbhtw0fq0jwsfb91nzb" \ -d "client_secret=xhcomab1myxac4vzi56rb0eoslh5bz" \ -d "grant_type=client_credentials" \ -d "scope=channel:read:redemptions user:read:email channel:read:subscriptions moderation:read"
-pasue()
-pause()
-pasue()
--- Tools/token_revoke.bat
+++
@@ -1,2 +0,0 @@
-curl -X POST "https://id.twitch.tv/oauth2/revoke" \ -d "client_id=ixz30kpcc77pvhy8cnqcrogju59igz" \  -d "token=m549p96oiiqbleqsr6c98dsyg4bnit"
-pause()
