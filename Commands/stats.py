f77dd053058844aeee8d0a532d6fe883f1edbff5
danndato <danieltova97@gmail.com>
2025-02-13 12:45:31 -0600

    Estableciendo punto de partida para la lectura de estadísticas en tiempo real

--- Commands/stats.py
+++ Commands/stats.py
@@ -209,7 +209,7 @@ def stats_commands(bot):
             user=ctx.author.name
         ranking =await get_stats("reto_wins",user,0)
         if ranking is not None:
-            await ctx.send(f"[BOT] - @{user} Ha ganado ({ranking}) reto{'s' if safe_int(ranking) >1 else ''}")
+            await ctx.send(f"[BOT] - @{user} Ha ganado ({ranking[1]}) reto{'s' if safe_int(ranking[1]) >1 else ''}")
         else:
             await ctx.send(f'[BOT] - Creo que @{user} nunca ha ganado un reto')
     bot.commands["retoscore"].category = "Retos"
