de5861bdd9eaadd4737c09725875676d574d02d2
Daniel Tovar <93622054+DannDato@users.noreply.github.com>
2026-01-29 20:02:58 +0000

    Se añaden comandos de TDT, info e IP - correccion

--- Commands/general.py
+++ Commands/general.py
@@ -35,12 +35,12 @@ class general_commands(commands.Component):
     """
 
     @commands.command(name='tdt')
-    async def lurk(self, ctx):
-        await ctx.send(f'[BOT] - Aquí está toda la información para entrar al servidor de miencraft https://dato.dannprod.com/tdt/info.html Tienes que leer las reglas para entender como funciona...')
+    async def tdt(self, ctx):
+        await ctx.send(f'[BOT] - Aquí está toda la información para entrar al servidor de miencraft TIERRA DE TODOS https://dato.dannprod.com/tdt/info.html Tienes que leer las reglas para entender como funciona...')
         await update_global_stats("xp_Oscuridad",ctx.chatter.id,3)
 
     @commands.command(name='iptdt')
-    async def lurk(self, ctx):
+    async def iptdt(self, ctx):
         await ctx.send(f'[BOT] - La ip de TDT es: tierradetodos.vultam.host ')
         await update_global_stats("xp_Oscuridad",ctx.chatter.id,3)
 
